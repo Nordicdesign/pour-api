fa08b595b0d3530d6e075073d8cdc853c0cc4c01
Alex Lillo <alex.lillo@gmail.com>
2022-04-05 21:28:46 +0100

    added sentry

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from 'express'
 // import bodyParser from 'body-parser';
 import morgan from 'morgan'
 import cors from 'cors'
+import * as Sentry from '@sentry/node'
+import * as Tracing from '@sentry/tracing'
 
 import authRoute from './routes/auth'
 import recipesRoute from './routes/recipes'
@@ -12,6 +14,29 @@ import { protect } from './controllers/auth/helpers/authHelpers'
 
 const app = express()
 
+Sentry.init({
+  dsn: 'https://404318ab40f2405ba504946211571b20@o551962.ingest.sentry.io/6311890',
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler())
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler())
+
+// All controllers should live here
+
 app.use(express.json())
 // app.use(bodyParser.json());
 app.use(function (req, res, next) {
@@ -19,9 +44,6 @@ app.use(function (req, res, next) {
   next()
 })
 app.options('*', cors())
-
-// request logging
-app.use(morgan('dev'))
 
 // ROUTES =============================================
 app.use('/auth', authRoute)
@@ -44,5 +66,20 @@ app.use((error, req, res) => {
   }
   return res.status(401).send('401: Unauthorized')
 })
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler())
+
+// Optional fallthrough error handler
+// eslint-disable-next-line node/handle-callback-err
+// app.use(function onError(err, req, res, next) {
+//   // The error id is attached to `res.sentry` to be returned
+//   // and optionally displayed to the user for support.
+//   res.statusCode = 500
+//   res.end(res.sentry + '\n')
+// })
+
+// request logging
+app.use(morgan('dev'))
 
 export default app
